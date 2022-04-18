40a1ead99ee1f7604a3b27c5b9e1e74446f25631
kyong-dev <kyong.dev@gmail.com>
2022-04-19 00:28:36 +0900

    input values array format

--- package/utils/mobile_number_format.py
+++ package/utils/mobile_number_format.py
@@ -3,10 +3,12 @@ def removeHyphen(phoneNumber: str):
 
 
 def addHyphen(phoneNumber: str):
-    phoneNumber = removeHyphen(phoneNumber)
-    phoneNumWithHyphen = f"{phoneNumber[0:-8]}-{phoneNumber[-8:-4]}-{phoneNumber[-4:]}"
-    # if the phone number has 10 digits and starts with 01
-    if phoneNumber[0:-8] == "01":
-        phoneNumWithHyphen = f"{phoneNumber[0:-7]}-{phoneNumber[-7:-4]}-{phoneNumber[-4:]}"
+    if len(phoneNumber) > 9:
+        phoneNumber = removeHyphen(phoneNumber)
+        phoneNumWithHyphen = f"{phoneNumber[0:-8]}-{phoneNumber[-8:-4]}-{phoneNumber[-4:]}"
+        # if the phone number has 10 digits and starts with 01
+        if phoneNumber[0:-8] == "01":
+            phoneNumWithHyphen = f"{phoneNumber[0:-7]}-{phoneNumber[-7:-4]}-{phoneNumber[-4:]}"
 
-    return phoneNumWithHyphen
+        return phoneNumWithHyphen
+    return phoneNumber
